f33297777f2609c8d9195de84df3ab24fd9645f2
Name <email@email.com>
2025-10-05 17:42:49 +0100

    Edited Autostart

--- resources/apps/autostart.sh
+++ resources/apps/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #  ~/.dwm/autostart.sh
 
-# feh --bg-fill /usr/share/backgrounds/blue-background.jpg
+feh --bg-fill /usr/share/backgrounds/blue-background.jpg
 pkill picom; picom --daemon
 pkill dwmblocks; dwmblocks &
+pkill xbindkeys; xbindkeys &
 kitty --hold=yes neofetch &
